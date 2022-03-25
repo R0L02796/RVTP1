ec18c0caf82d862854828017b67597df50fe30da
facu9812 <77784314+facu9812@users.noreply.github.com>
2022-03-25 10:47:12 -0300

    update to movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Movement : MonoBehaviour
 {
-
+    private XRRig rig;
     public CharacterController controller;
     public GameObject camera;
     public float speed = 12f;
@@ -14,6 +16,10 @@ public class Movement : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        Quaternion headyaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        
+
+        
 
         Vector3 move = camera.transform.right * x + camera.transform.forward * z;
 
